a37beaf10e96cac81548dd0f63d6d2d30c8c5ce5
nicola <nicholas.dipasquale@gmail.com>
2023-02-25 10:15:38 -0500

    Updated validation functions for endgame result

--- validate.c
+++ validate.c
@@ -50,3 +50,80 @@ bool ce_valid_piece_empty(const int32_t pce) {
 bool ce_valid_piece(const int32_t pce) {
   return (pce >= wP && pce <= bK) ? true : false;
 }
+
+
+/**
+ * Chess Engine validation function for the fifty move rule
+ * @param pos The current board position
+ * @return Boolean value indicating if the fifty move rule has been invoked
+ */
+bool ce_is_fifty_move(const struct board_s *pos) {
+  return pos->fiftyMove > 100;
+}
+
+/**
+ * Chess Engine validation function for a position with a three fold repetition
+ * @param pos The current board position
+ * @return Boolean value indicating if the board position represents a three fold repetition
+ */
+bool ce_is_three_fold_repetition(const struct board_s *pos) {
+  int32_t i = 0, r = 0;
+  for (i = 0; i < pos->historyPly; ++i) {
+    if (pos->history[i].positionKey == pos->positionKey) {
+      ++r;
+    }
+  }
+  return r > 2;
+}
+
+/**
+ * Chess Engine valiation function for a position with insufficient material
+ * @param pos The current board position
+ * @return Boolean value indicating if the board position represents a position with insufficient material
+ */
+bool ce_is_draw_material(const struct board_s *pos) {
+  if (pos->pieceNum[wP] || pos->pieceNum[bP]) {
+    return false;
+  }
+  if (pos->pieceNum[wQ] || pos->pieceNum[bQ] || pos->pieceNum[wR] || pos->pieceNum[bR]) {
+    return false;
+  }
+  if (pos->pieceNum[wB] > 1 || pos->pieceNum[bB] > 1) {
+    return false;
+  }
+  if (pos->pieceNum[wN] > 1 || pos->pieceNum[bN] > 1) {
+    return false;
+  }
+  if (pos->pieceNum[wN] && pos->pieceNum[wB]) {
+    return false;
+  }
+  if (pos->pieceNum[bN] && pos->pieceNum[bB]) {
+    return false;
+  }
+  return true;
+}
+
+/**
+ * Chess Engine validation function for a checkmated position
+ * @param pos The current board position
+ * @return Boolean value indicating if the board position represents a checkmate
+ */
+bool ce_is_moves_available(const struct board_s *pos) {
+  struct board_s b = { 0 };
+  struct move_list_s list = { 0 };
+  bool found = false;
+
+  // XXX: I think this makes every movegen call that much more expensive.
+  memcpy((void *) &b, (void *) pos, sizeof(struct board_s));
+
+  ce_generate_all_moves(&b, &list);
+  for (size_t moveNum = 0; moveNum < list.count; ++moveNum) {
+    if (!ce_move_make(&b, list.moves[moveNum].move.val)) {
+      continue;
+    }
+    ce_move_take(&b);
+    return true;
+  }
+
+  return false;
+}
--- xboard.c
+++ xboard.c
@@ -1,71 +1,26 @@
 
 #include "defs.h"
 
-int32_t ce_three_fold_repetition(const struct board_s *pos) {
-  int32_t i = 0, r = 0;
-  for (i = 0; i < pos->historyPly; ++i) {
-    if (pos->history[i].positionKey == pos->positionKey) {
-      ++r;
-    }
-  }
-  return r;
-}
-
-int32_t ce_draw_material(const struct board_s *pos) {
-  if (pos->pieceNum[wP] || pos->pieceNum[bP]) {
-    return false;
-  }
-  if (pos->pieceNum[wQ] || pos->pieceNum[bQ] || pos->pieceNum[wR] || pos->pieceNum[bR]) {
-    return false;
-  }
-  if (pos->pieceNum[wB] > 1 || pos->pieceNum[bB] > 1) {
-    return false;
-  }
-  if (pos->pieceNum[wN] > 1 || pos->pieceNum[bN] > 1) {
-    return false;
-  }
-  if (pos->pieceNum[wN] && pos->pieceNum[wB]) {
-    return false;
-  }
-  if (pos->pieceNum[bN] && pos->pieceNum[bB]) {
-    return false;
-  }
-  return true;
-}
-
 bool ce_check_result(struct board_s *pos) {
-  struct move_list_s list = { 0 };
-  int32_t found = 0;
-
-  if (pos->fiftyMove > 100) {
+  if (ce_is_fifty_move(pos)) {
     printf(u8"1/2-1/2 (fifty move rule)\n");
     return true;
   }
-  if (ce_three_fold_repetition(pos) >= 2) {
+  if (ce_is_three_fold_repetition(pos)) {
     printf(u8"1/2-1/2 (3-fold repetition)\n");
     return true;
   }
-  if (ce_draw_material(pos) == true) {
+  if (ce_is_draw_material(pos)) {
     printf(u8"1/2-1/2 (insuficient material)\n");
     return true;
   }
 
-  ce_generate_all_moves(pos, &list);
-  for (size_t moveNum = 0; moveNum < list.count; ++moveNum) {
-    if (!ce_move_make(pos, list.moves[moveNum].move)) {
-      continue;
-    }
-    ++found;
-    ce_move_take(pos);
-    break;
-  }
-
-  if (found != 0) {
+  if (ce_is_moves_available(pos)) {
     return false;
   }
 
   bool inCheck = ce_is_square_attacked(pos->kingSq[pos->side], pos->side ^ 1, pos);
-  if (inCheck == true) {
+  if (inCheck) {
     if (pos->side == WHITE) {
       printf(u8"0-1 (black mate)\n");
       return true;
@@ -78,6 +33,7 @@ bool ce_check_result(struct board_s *pos) {
     printf(u8"\n1/2-1/2 (stalemate)\n");
     return true;
   }
+
   return false;
 }
 
